6972df99731a8e778c1835485e27513f4033c726
sachin Gupta <sachinguptasg7633780@gmail.com>
2025-03-31 14:24:03 +0530

    Rendering with some message

--- tasks/src/components/task6/ConditionalRen.js
+++ tasks/src/components/task6/ConditionalRen.js
@@ -1,6 +1,29 @@
 //This will show welcome user when user was loggedin by default user is not loggedIn
 //So make the user login we have created button in which we are setting the default false to true ...
-//when that value become true user will goggedIn.
+//when that value become true user will LoggedIn.
+// import { useState } from "react"
+
+// function ConditionalRen(){
+
+//     const[isLoggedIn, setIsLoggedIn] = useState(false);
+
+//     return(
+//         <>
+//         {
+//             isLoggedIn ? <h2>Welcome !user</h2> : 
+//             <button onClick={()=>{setIsLoggedIn(true)}}>LogIn</button>
+//         }
+//         </>
+//     )
+// }
+
+// export default ConditionalRen;
+
+
+//We can make it as toggle as well ................. see how ,
+//Nothing just, Whenever user clicks button and he is not loggedin then he will lgoin other wise
+//He is loggedIn already then he will loggedOut. 
+//Inside ternary o/p we will pass one another button instead of  welcome user message.
 import { useState } from "react"
 
 function ConditionalRen(){
@@ -9,12 +32,17 @@ function ConditionalRen(){
 
     return(
         <>
+        
         {
-            isLoggedIn ? <h2>Welcome !user</h2> : 
+            isLoggedIn ? <button onClick={()=>{setIsLoggedIn(false)}}>LogOut</button>: 
             <button onClick={()=>{setIsLoggedIn(true)}}>LogIn</button>
         }
+        <h2>{isLoggedIn ? "User is Logged In" : "Please Login to continue!."}</h2>
+        
         </>
     )
 }
 
 export default ConditionalRen;
+
+//Have you seen what i did, I just did conditional rendering.
